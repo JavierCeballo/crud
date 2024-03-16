b4d033201a5668a4a79b8762d73fafce4d2c5964
Javier Ceballo <jvrceballo@gmail.com>
2024-03-16 18:07:19 -0400

    complete create

--- blogs/models.py
+++ blogs/models.py
@@ -15,5 +15,5 @@ class Publication(models.Model):
         return self.title
     
     def get_absolute_url(self):
-        return reverse("publications:detail", kwargs={"pk": self.pk})
+        return reverse("post_detail", kwargs={"pk": self.pk})
 
