bef2912265734dfd56dff8f35dee00b18ba3130c
Javier Ceballo <jvrceballo@gmail.com>
2024-04-06 16:46:18 -0400

    login

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local django
     "blogs.apps.BlogsConfig",
 ]
 
@@ -123,3 +124,5 @@ STATICFILES_DIRS = [ BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
